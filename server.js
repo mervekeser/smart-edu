3d3c756c6ca88a086b19ba09c91b3e9461096c0f
Merve Keser <mervekeser8@outlook.com>
2025-04-11 11:44:46 +0300

    home about page ejs

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello Smart Edu");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Serve has started on port ${port}`);
+  console.log(`Server has started on port ${port}`);
 });
