a092a98fd2747137199722cebc215e7e8d6c3703
Merve Keser <mervekeser8@outlook.com>
2025-04-15 12:52:26 +0300

    user register

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 const connectDB = require("./db/connect");
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = process.env.PORT || 3000;
 
